04c7eeb7671b8a9a2a40e2d95b68d282b0a73880
doushe821 <romachevskii.ae@phystech.edu>
2025-10-26 16:16:47 +0300

    Removed useless code in write and read functions

--- sim/src/spu.cpp
+++ sim/src/spu.cpp
@@ -65,83 +65,13 @@ namespace ToySim {
   void SPU::SPUMemory::write(const void* Source, unsigned N, unsigned DestinationAddress) {
     assert(Source && "Invalid source pointer\n");
     assert((DestinationAddress < MemorySize && DestinationAddress + N < MemorySize) && "Trying to write to unreachable address\n");
-    unsigned SourceBufferIndex {0};
-    while (N >= kOctWordSize) {
-      memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress + SourceBufferIndex, Source, kOctWordSize);
-      N -= kOctWordSize;
-      SourceBufferIndex += kOctWordSize;
-    }
-
-    while (N >= kQuadWordSize) {
-      memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress + SourceBufferIndex, Source, kQuadWordSize);
-      N -= kQuadWordSize;
-      SourceBufferIndex += kQuadWordSize;
-    }
-
-    while (N >= kDoubleWordSize) {
-      memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress + SourceBufferIndex, Source, kDoubleWordSize);
-      N -= kDoubleWordSize;
-      SourceBufferIndex += kDoubleWordSize;
-    }
-
-    while (N >= kWordSize) {
-      memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress + SourceBufferIndex, Source, kWordSize);
-      N -= kWordSize;
-      SourceBufferIndex += kWordSize;
-    }
-
-    while (N >= kHalfWordSize) {
-      memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress + SourceBufferIndex, Source, kHalfWordSize);
-      N -= kHalfWordSize;
-      SourceBufferIndex += kHalfWordSize;
-    }
-    
-    while (N >= kByteSize) {
-      memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress + SourceBufferIndex, Source, kByteSize);
-      N -= kByteSize;
-      SourceBufferIndex += kByteSize;
-    }
+    memcpy(reinterpret_cast<char*>(Memory.data()) + DestinationAddress, Source, N);
   }
 
   void SPU::SPUMemory::read(void* Destination, unsigned N, unsigned SourceAddress) const {
     assert(Destination && "Invalid destination pointer\n");
     assert((SourceAddress < MemorySize && SourceAddress + N < MemorySize) && "Trying to read from unreachable address\n");
-    unsigned DestBufferIndex {0};
-    while (N >= kOctWordSize) {
-      memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress + DestBufferIndex, kOctWordSize);
-      N -= kOctWordSize;
-      DestBufferIndex += kOctWordSize;
-    }
-
-    while (N >= kQuadWordSize) {
-      memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress + DestBufferIndex, kQuadWordSize);
-      N -= kQuadWordSize;
-      DestBufferIndex += kQuadWordSize;
-    }
-
-    while (N >= kDoubleWordSize) {
-      memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress + DestBufferIndex, kDoubleWordSize);
-      N -= kDoubleWordSize;
-      DestBufferIndex += kDoubleWordSize;
-    }
-
-    while (N >= kWordSize) {
-      memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress + DestBufferIndex, kWordSize);
-      N -= kWordSize;
-      DestBufferIndex += kWordSize;
-    }
-
-    while (N >= kHalfWordSize) {
-      memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress + DestBufferIndex, kHalfWordSize);
-      N -= kHalfWordSize;
-      DestBufferIndex += kHalfWordSize;
-    }
-    
-    while (N >= kByteSize) {
-      memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress + DestBufferIndex, kByteSize);
-      N -= kByteSize;
-      DestBufferIndex += kByteSize;
-    }
+    memcpy(Destination, reinterpret_cast<const char*>(Memory.data()) + SourceAddress, N);
   }
 
 
